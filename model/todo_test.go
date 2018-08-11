efad44dfd337beb69c56ca1512eb99792ad42aa6
SrcHndWng <UnderSourceCode@hotmail.co.jp>
2018-08-11 20:35:08 +0900

    ユニットテスト用のテーブルを作成。

--- model/todo_test.go
+++ model/todo_test.go
@@ -6,15 +6,20 @@ import (
 )
 
 func TestCreate(t *testing.T) {
-	todo := NewTodo()
-	err := todo.Create("test task.")
-	if err != nil {
-		t.Fatalf("error raise. %#v", err)
+	todo := newTodoTest()
+	clear(todo)
+	test := func(task string) {
+		err := todo.Create(task)
+		if err != nil {
+			t.Fatalf("error raise. %#v", err)
+		}
 	}
+	test("test task.")
+	test("test task2.")
 }
 
 func TestList(t *testing.T) {
-	todo := NewTodo()
+	todo := newTodoTest()
 	todos, err := todo.List()
 	if err != nil {
 		t.Fatalf("error raise. %#v", err)
@@ -23,22 +28,22 @@ func TestList(t *testing.T) {
 }
 
 func TestIsExist(t *testing.T) {
-	todo := NewTodo()
+	todo := newTodoTest()
 	result, err := todo.IsExist(2)
 	if err != nil {
 		t.Fatalf("error raise. %#v", err)
 	}
-	fmt.Printf("id exists. result = %v\n", result)
+	fmt.Printf("id 2 exists. result = %v\n", result)
 
 	result, err = todo.IsExist(0)
 	if err != nil {
 		t.Fatalf("error raise. %#v", err)
 	}
-	fmt.Printf("id not exists. result = %v\n", result)
+	fmt.Printf("id 0 not exists. result = %v\n", result)
 }
 
 func TestGet(t *testing.T) {
-	todo := NewTodo()
+	todo := newTodoTest()
 	data, err := todo.Get(2)
 	if err != nil {
 		t.Fatalf("error raise. %#v", err)
@@ -47,7 +52,7 @@ func TestGet(t *testing.T) {
 }
 
 func TestUpdate(t *testing.T) {
-	todo := NewTodo()
+	todo := newTodoTest()
 	err := todo.Update(2, "test update")
 	if err != nil {
 		t.Fatalf("error raise. %#v", err)
@@ -55,9 +60,26 @@ func TestUpdate(t *testing.T) {
 }
 
 func TestDelete(t *testing.T) {
-	todo := NewTodo()
+	todo := newTodoTest()
 	err := todo.Delete(2)
 	if err != nil {
 		t.Fatalf("error raise. %#v", err)
 	}
+}
+
+func newTodoTest() *Todo {
+	t := Todo{}
+	t.table = Table("go_todos_test")
+	return &t
+}
+
+func clear(t *Todo) (err error) {
+	var todos []Todo
+	if todos, err = t.List(); err != nil {
+		return err
+	}
+	for _, todo := range todos {
+		t.Delete(todo.ID)
+	}
+	return nil
 }
